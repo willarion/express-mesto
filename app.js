0e592b0476b9ca16822c4c85747e6a2b7a2f2f4a
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-12-02 17:45:18 +0300

    feature: добавить статику

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+
