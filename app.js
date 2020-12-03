5f7d7e878ceadb8482e483cc42941c7ede777831
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-12-03 21:29:15 +0300

    feature: сделать роутинг к пользователям и на неизвестный адрес

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const usersRouter = require('./routes/users');
 const PORT = 3000;
 
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', usersRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
+});
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
