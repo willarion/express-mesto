a617b6810729e07bee39208c67ed6e0ad1ced62e
Marina Anisimova <anisimova.marina.n@gmail.com>
2020-12-18 01:56:36 +0300

    feature: добавить валидацию ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,12 @@
 const Card = require('../models/card');
+const { errorCodes } = require('../utils/constants');
 
 
 function getCards(req, res) {
   Card.find({})
     .populate('owner')
     .then(cards => res.send(cards))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка', error: err }));
+    .catch(err => res.status(errorCodes.ERROR_SERVER).send({ message: 'Произошла ошибка на сервере', error: err }));
 }
 
 function createCard(req, res) {
@@ -13,14 +14,32 @@ function createCard(req, res) {
 
   Card.create({name, link, owner: req.user._id })
     .then(card => res.send({ data: card }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка', error: err }));
+    .catch((err) => {
+      if (err.name === 'ValidatorError') {
+        return res.status(errorCodes.ERROR_INVALID_ID).send({ message: 'Введённые данные невалидны' })
+      }
+      res.status(errorCodes.ERROR_SERVER).send({ message: 'Произошла ошибка на сервере', error: err })
+    });
 }
 
 function deleteCard(req, res) {
 
   Card.findByIdAndRemove(req.params.cardId)
-  .then(card => res.send({ data: card, message: "карточка удалена" }))
-  .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+    .orFail(() => {
+      const err = new Error();
+      err.statusCode = errorCodes.ERROR_NOT_FOUND;
+      throw err;
+    })
+    .then(card => res.send({ data: card, message: "карточка удалена" }))
+    .catch((err) => {
+      if(err.kind === 'ObjectId') {
+        return res.status(errorCodes.ERROR_INVALID_ID).send({ message: 'Невалидный id карточки' })
+      }
+      if (err.statusCode === errorCodes.ERROR_NOT_FOUND) {
+        return res.status(errorCodes.ERROR_NOT_FOUND).send({ message: 'Карточки с таким id не существует' })
+      }
+      res.status(errorCodes.ERROR_SERVER).send({ message: 'Произошла ошибка на сервере', error: err })
+    });
 }
 
 
--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,30 @@
 const User = require('../models/user');
+const { errorCodes } = require('../utils/constants');
 
 
 function getUsers(req, res) {
   User.find({})
     .then(users => res.send(users))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка', error: err }));
+    .catch(err => res.status(errorCodes.ERROR_SERVER).send({ message: 'Произошла ошибка на сервере', error: err }));
 }
 
 function getUserProfile(req, res) {
   User.findById(req.params.id)
+    .orFail(() => {
+      const err = new Error();
+      err.statusCode = errorCodes.ERROR_NOT_FOUND;
+      throw err;
+    })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка', error: err }));
+    .catch((err) => {
+      if(err.kind === 'ObjectId') {
+        return res.status(errorCodes.ERROR_INVALID_ID).send({ message: 'Невалидный id пользователя' })
+      }
+      if (err.statusCode === errorCodes.ERROR_NOT_FOUND) {
+        return res.status(errorCodes.ERROR_NOT_FOUND).send({ message: 'Пользователя с таким id не существует' })
+      }
+      res.status(errorCodes.ERROR_SERVER).send({ message: 'Произошла ошибка на сервере', error: err })
+    });
 }
 
 function createUser(req, res) {
@@ -18,7 +32,12 @@ function createUser(req, res) {
 
   User.create({ name, about, avatar })
     .then(user => res.send({ data: user }))
-    .catch(err => res.status(500).send({ message: 'Произошла ошибка', error: err }));
+    .catch((err) => {
+      if (err.name === 'ValidatorError') {
+        return res.status(errorCodes.ERROR_INVALID_ID).send({ message: 'Введённые данные невалидны' })
+      }
+      res.status(errorCodes.ERROR_SERVER).send({ message: 'Произошла ошибка на сервере', error: err })
+    });
 }
 
 
